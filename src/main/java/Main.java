449d9a603e927e60f5aab5f31cd4212a62ce1ac1
Arsen Baktiyarov <baktiyarov21@gmail.com>
2021-02-02 11:19:38 +0600

    Init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,5 +7,8 @@ public class Main {
         for (int i = 0; i < 100; i++) {
             linkedList.add(i);
         }
+        for (int i = 0; i < 100; i++) {
+            linkedList.add(i);
+        }
     }
 }
